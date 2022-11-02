33503e6a51785ede36777bbd388deabcc9e76280
Dani Ryuu Carmona <DaniiCarmona22@gmail.com>
2022-11-02 21:26:48 +0900

    tidied up replacer

--- src/replacer.js
+++ src/replacer.js
@@ -5,27 +5,7 @@ module.exports = {
             const currentKey = Object.keys(jsonData)[i];
             // Key is an array of object
             if(jsonData[currentKey].constructor === Array) {
-                let tempString = "";
-
-                if (currentKey === "languages") {
-                    for (let j = 0; j < jsonData[currentKey].length; j++) {
-                        const element = jsonData[currentKey][j];
-
-                        tempString += createLanguageBlock(element.lang, element.proficiency);
-                    }
-                }
-                else if (currentKey === "references") {
-                    for (let j = 0; j < jsonData[currentKey].length; j++) {
-                        const element = jsonData[currentKey][j];
-                        tempString += createReferenceBlock(element.name, element.contact);
-                    }      
-                }
-                else {
-                    for (let j = 0; j < jsonData[currentKey].length; j++) {
-                        const element = jsonData[currentKey][j];
-                        tempString += element;
-                    }
-                }
+                const tempString = replaceArray(currentKey,jsonData[currentKey]);
                 tempCopy = findAndReplace(tempCopy, {
                     key : currentKey,
                     string : tempString
@@ -53,6 +33,32 @@ function findAndReplace(template, object) {
     const key = "${" + object.key + "}";
     template = template.replaceAll(key, object.string);
     return template;
+}
+
+function replaceArray(currentKey, arr) {
+    let tempString = "";
+
+    if (currentKey === "languages") {
+        for (let j = 0; j < arr.length; j++) {
+            const element = arr[j];
+
+            tempString += createLanguageBlock(element.lang, element.proficiency);
+        }
+    }
+    else if (currentKey === "references") {
+        for (let j = 0; j < arr.length; j++) {
+            const element = arr[j];
+            tempString += createReferenceBlock(element.name, element.contact);
+        }      
+    }
+    else {
+        for (let j = 0; j < arr.length; j++) {
+            const element = arr[j];
+            tempString += element;
+        }
+    }
+    return tempString;
+    
 }
 
 function createLanguageBlock(lang,level) {
